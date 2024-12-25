47baa074f5051797ecf33cd366ff3d1dfc9d16a6
Denzel1806 <holyrodz68@gmail.com>
2024-12-24 16:50:47 -0900

    Add files via upload

---
+++ Datatypesandvariables.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypesandvariables;
+
+/**
+ *
+ * @author admin
+ */
+public class Datatypesandvariables {
+
+    public static void main(String[] args) {
+        
+        // Integer types
+        byte byteValue = 100;         // 8-bit integer
+        short shortValue = 15000;     // 16-bit integer
+        int intValue = 100000;        // 32-bit integer
+        long longValue = 10000000000L; // 64-bit integer (L is used to denote long literals)
+
+        // Floating-point types
+        float floatValue = 3.14f;      // 32-bit floating-point number (f is used to denote float literals)
+        double doubleValue = 3.14159265359; // 64-bit floating-point number
+
+        // Character type
+        char charValue = 'A';         // 16-bit Unicode character
+
+        // Boolean type
+        boolean booleanValue = true;  // true or false value
+        
+        // Printing values
+        System.out.println("Byte value: " + byteValue);
+        System.out.println("Short value: " + shortValue);
+        System.out.println("Int value: " + intValue);
+        System.out.println("Long value: " + longValue);
+        System.out.println("Float value: " + floatValue);
+        System.out.println("Double value: " + doubleValue);
+        System.out.println("Char value: " + charValue);
+        System.out.println("Boolean value: " + booleanValue);
+    }
+}
+
+    
+    
+
